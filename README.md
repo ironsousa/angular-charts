c286bf380ce9e91423cead5bd150c8cd24274f4e
Iron Sousa <ironsousa@gmail.com>
2022-01-13 22:53:13 -0300

    Adding Angular Charts Example.

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # angular-charts
+
+## CONFIG STEPS
+
+```
+1. sudo npm install -g @angular/cli
+2. ng new charts
+3. cd charts
+4. npm install chart.js --save
+5. npm install --save rxjs-compat
+6. npm install -g json-server
+```
+
+## ADDING DATA TO FAKE JSON-SERVER
+
+```
+gitpod /workspace/angular-charts/charts $ cat > data.json
+{
+  "results": [
+  {
+      "month": "Jan",
+      "price": "180"
+  },
+  {
+    "month": "Feb",
+    "price": "200"
+  },
+  {
+    "month": "March",
+    "price": "210"
+  },
+  {
+    "month": "April",
+    "price": "190"
+  },
+  {
+    "month": "May",
+    "price": "240"
+  },
+  {
+    "month": "June",
+    "price": "230"
+  },
+  {
+    "month": "July",
+    "price": "260"
+  },
+  {
+    "month": "Aug",
+    "price": "210"
+  },
+  {
+    "month": "Sept",
+    "price": "300"
+  }]
+}
+```
+## Starting Server
+
+```
+json-server --watch data.json --port 4000
+```
+
+### OUTPUT
+```
+  Resources
+  http://localhost:4000/results
+
+  Home
+  http://localhost:4000
+```
---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+import { HttpClient } from '@angular/common/http';
+import { Data } from './Data';
+import { error } from '@angular/compiler/src/util';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  data: Data[] = Array();
+  url = 'http://localhost:4000/results';
+  month = Array();
+  price = Array();
+  chart!: Chart;
+  
+  constructor(private httpClient: HttpClient) {
+	Chart.register(...registerables);  
+  }
+
+  ngOnInit() {
+    
+    this.price.push(-1);
+    this.httpClient.get<Data[]>(this.url).subscribe({
+        next: (res: Data[]) => {
+			res.forEach((y: Data) => {
+			  this.month.push(y.month);
+			  this.price.push(y.price);
+			});
+			this.chart = new Chart('canvas', {
+			  type: 'line',
+			  data: {
+				labels: this.month,
+				datasets: [
+				  {
+					data: this.price,
+					borderColor: '#3cba9f',
+					fill: false
+				  }
+				]
+			  },
+			  options: {
+				scales: {
+					x: {
+						display: true
+					},
+					y: {
+						display: true
+					}
+				},
+				plugins: {
+					legend: {
+						display: false
+					}
+				}
+			  }
+			});
+		},
+        error: (e) => console.log(e),
+        complete: () => console.info()
+    });
+    
+  }
+}
